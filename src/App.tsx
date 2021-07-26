5a5fb3022db22d8d9ecadf2c368a9564859030ef
KANDA, Tetsuya <t-kanda@ist.osaka-u.ac.jp>
2021-07-26 17:58:00 +0900

    setup environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" className="title">
+            didiffff
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reloadoooo.
         </p>
         <a
           className="App-link"
