93ed933d7e68d48edd8e489c88ef9acea328ad84
KANDA, Tetsuya <t-kanda@ist.osaka-u.ac.jp>
2021-07-26 19:10:20 +0900

    win

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
-import './App.css';
-
 function App() {
   return (
     <div className="App">
